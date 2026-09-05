d1be006ccb3f65dd5b3a9ad9284f9107ca058f88
amama-dalvi <amamadalvi01@gmail.com>
2026-09-05 17:28:39 +0530

    Inventory APIs Added

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const rateLimit = require("express-rate-limit");
-const profileRoutes = require("./routes/profile.routes");
+
+
 const authRoutes = require("./routes/auth.routes");
 const adminRoutes = require("./routes/admin.routes");
 const productRoutes = require("./routes/product.routes");
 const adminProductRoutes = require("./routes/admin.product.routes");
-
-
+const profileRoutes = require("./routes/profile.routes");
+const inventoryRoutes = require("./routes/inventory.routes");
+const adminInventoryRoutes = require("./routes/admin.inventory.routes");
 
 
 const env = require("./config/env");
@@ -113,6 +115,8 @@ app.use(`${API_PREFIX}/admin`, adminRoutes);
 app.use("/api/profiles", profileRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/admin/products", adminProductRoutes);
+app.use("/api/inventory", inventoryRoutes);
+app.use("/api/admin/inventory", adminInventoryRoutes);
 /*
 |--------------------------------------------------------------------------
 | 404 Handler
